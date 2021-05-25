78aa29d80a3b971f24eac23a30e07aaa0a9ac7a0
John Wason <wason@wasontech.com>
2021-05-25 15:07:07 -0400

    Set CMAKE_INSTALL_LIBDIR=lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p build2
 cd build2
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX -DCMAKE_BUILD_TYPE:STRING=Release ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_LIBDIR=lib ..
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
