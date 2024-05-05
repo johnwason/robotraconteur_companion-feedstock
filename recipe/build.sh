b3ddc80e4231915fcb6b48b5865081a5b22fc1f7
John Wason <wason@wasontech.com>
2024-05-05 19:27:53 -0400

    Use RobotRaconteurGen from "build" environment when cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p build2
 cd build2
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ]]; then
   export CMAKE_CROSS_ARGS=-DROBOTRACONTEUR_GEN_EXECUTABLE=${BUILD_PREFIX}/bin/RobotRaconteurGen
 fi
 
